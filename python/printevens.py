960087af641f07f8583c7a78dc1617ad94a4a0cd
Abhijith K Manoj <abhijithkmanoj46@gmail.com>
2025-11-18 11:43:37 +0530

    Update printevens.py
    Add functionality to print even numbers from a list until 237 is reached.

--- python/printevens.py
+++ python/printevens.py
@@ -1,2 +1,10 @@
 # 4.	Write a program to print all even numbers from a given list in the given order until you reach number 237 or end of the list. 
 
+
+numbers = list(map(int, input("Enter numbers separated by space: ").split()))
+
+for num in numbers:
+    if num == 237:
+        break  
+    if num % 2 == 0:
+        print(num)
